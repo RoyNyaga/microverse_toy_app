20a2b1370ba5678f1dc445acb53c96ef594fbca6
Roy Nyaga <nyagaandreroy@gamil.com>
2019-09-06 02:38:41 +0100

    done with toy app

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+	has_many :microposts
+	has_many :microposts
+  	validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+  	validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+end
